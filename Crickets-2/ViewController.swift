c702402e1cb58aed15e75258c68dc7d99a9fcd2a
brickatyourfeet <brickatyourfeet@gmail.com>
2019-10-08 20:50:55 -0700

    Initial Commit

---
+++ Crickets-2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Crickets-2
+//
+//  Created by Kyle Braden on 10/8/19.
+//  Copyright © 2019 Kyle Braden. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
